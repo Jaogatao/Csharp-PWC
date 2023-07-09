9904d560d3e82eda86878585ee47cc9a5cccbeb3
Jaogatao <playgreen20pg@gmail.com>
2023-07-08 22:07:05 -0300

    1 exercício

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//1° Exercício 
+
+string texto = "Hello, World OpenAI is amazing";
+string[] palavras = texto.Split(' ');
+string textoInvertido = "";
+
+for (int i = palavras.Length - 1; i >= 0; i--)
+{
+    textoInvertido += palavras[i];
+
+    if (i != 0)
+    {
+        textoInvertido += " ";
+    }
+}
+
+
+//2° exercício 
+
+
+
