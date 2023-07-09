d2611e146acdfec14feb83b9840fd99ed6d65584
Jaogatao <playgreen20pg@gmail.com>
2023-07-09 16:20:16 -0300

    4° e 5° exercicio

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿//1° Exercício 
+﻿
+//1° Exercício 
 
  string texto1 = "Hello, World OpenAI is amazing";
  string[] palavras = texto1.Split(' ');
@@ -34,9 +35,47 @@ Console.WriteLine(novoTexto);
 
 
 //3° exercício
-
-
-
+{
+    static void Main(string[] args)
+    {
+        string word = "arara";
+        List<string> palindromicSubstrings = GeneratePalindromicSubstrings(word);
+        
+        Console.WriteLine("Substrings palindrômicas:");
+        foreach (string substring in palindromicSubstrings)
+        {
+            Console.WriteLine(substring);
+        }
+    }
+    
+    static List<string> GeneratePalindromicSubstrings(string word)
+    {
+        List<string> palindromicSubstrings = new List<string>();
+        
+        for (int i = 0; i < word.Length; i++)
+        {
+            for (int j = i + 1; j <= word.Length; j++)
+            {
+                string substring = word.Substring(i, j - i);
+                if (IsPalindrome(substring))
+                {
+                    palindromicSubstrings.Add(substring);
+                }
+            }
+        }
+        
+        return palindromicSubstrings;
+    }
+    
+    static bool IsPalindrome(string word)
+    {
+        char[] characters = word.ToCharArray();
+        Array.Reverse(characters);
+        string reversedWord = new string(characters);
+        
+        return word.Equals(reversedWord, StringComparison.OrdinalIgnoreCase);
+    }
+}
 
 
 
@@ -44,11 +83,37 @@ Console.WriteLine(novoTexto);
 
 
 //4° Exercício
+var texto4 = "hello, how are you ? i`m fine, thank you.";
+var upper = char.ToUpper(texto4[0]) + texto4.Substring(1);
 
-        string input = "hello, how are you ? i`m fine, thank you.";
-        string output = (input.ToLower());
+Console.WriteLine(texto4);
+
+//5 exercício
+
+{
+    static void Main(string[] args)
+    {
+        string word = "racecar";
+        bool isPalindrome = IsPalindrome(word);
         
-        Console.WriteLine(output);
-
-
+        if (isPalindrome)
+        {
+            Console.WriteLine($"{word} é um palíndromo.");
+        }
+        else
+        {
+            Console.WriteLine($"{word} não é um palíndromo.");
+        }
+    }
+    
+    static bool IsPalindrome(string word)
+    {
+        char[] characters = word.ToCharArray();
+        Array.Reverse(characters);
+        string reversedWord = new string(characters);
+        
+        return word.Equals(reversedWord, StringComparison.OrdinalIgnoreCase);
+    }
+}
+     
 
